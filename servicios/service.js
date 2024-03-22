bacaf2e68ef7681f0d5897412103a22b65fd000f
Allison Briñez <allison.mitchelle@gmail.com>
2024-03-22 11:13:51 -0500

    nuevo puerto

--- servicios/service.js
+++ servicios/service.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const crypto = require('crypto');
 
 const app = express();
-// const port = process.env.port || 3306;
+const port = process.env.port || 3306;
 
 app.use(cors());
 app.use(bodyParser.json());
